3efbdd1fa181ea08143d349db8400960defb54fc
sunjikai <sunjikai@hiynn.com>
2019-05-29 18:29:12 +0800

    源码解析

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringApplication.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/SpringApplication.java
@@ -316,55 +316,55 @@ public class SpringApplication {
 	 */
 	// main方法的入口类
 	public ConfigurableApplicationContext run(String... args) {
-		// <1> 创建 StopWatch 对象，并启动。StopWatch 主要用于简单统计 run 启动过程的时长。
+		//创建 StopWatch 对象，并启动。StopWatch 主要用于简单统计 run 启动过程的时长。
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 		ConfigurableApplicationContext context = null;
 		Collection<SpringBootExceptionReporter> exceptionReporters = new ArrayList<>();
-		// <2> 配置 headless 属性
+		//配置 headless 属性
 		configureHeadlessProperty();
 		// 获得 SpringApplicationRunListener 的数组，并启动监听
 		SpringApplicationRunListeners listeners = getRunListeners(args);
 		//启动 在在getRunListeners中 的构造参数中已经初始化好了List<SpringApplicationRunListener>
 		listeners.starting();
 		try {
-			// <3> 创建 ApplicationArguments 对象
+			//创建 ApplicationArguments 对象
 			ApplicationArguments applicationArguments = new DefaultApplicationArguments(
 					args);
-			// <4> 加载属性配置。执行完成后，所有的 environment 的属性都会加载进来，包括 application.properties
+			// 加载属性配置。执行完成后，所有的 environment 的属性都会加载进来，包括 application.properties
 			// 和外部的属性配置
 			ConfigurableEnvironment environment = prepareEnvironment(listeners,
 					applicationArguments);
 			configureIgnoreBeanInfo(environment);
-			// <5> 打印 Spring Banner
+			// 打印 Spring Banner
 			Banner printedBanner = printBanner(environment);
-			// <6> 创建 Spring 容器。
+			//创建 Spring 容器。
 			context = createApplicationContext();
-			// <7> 异常报告器
+			//异常报告器
 			exceptionReporters = getSpringFactoriesInstances(
 					SpringBootExceptionReporter.class,
 					new Class[] { ConfigurableApplicationContext.class }, context);
-			// <8> 主要是调用所有初始化类的 ApplicationContextInitializer initialize 方法
+			//主要是调用所有初始化类的 ApplicationContextInitializer initialize 方法
 			prepareContext(context, environment, listeners, applicationArguments,
 					printedBanner);
-			// <9> 初始化 Spring 容器。
+			//初始化 Spring 容器。
 			refreshContext(context);
-			// <10> 执行 Spring 容器的初始化的后置逻辑。默认实现为空。
+			//执行 Spring 容器的初始化的后置逻辑。默认实现为空。
 			afterRefresh(context, applicationArguments);
-			// <11> 停止 StopWatch 统计时长
+			//停止 StopWatch 统计时长
 			stopWatch.stop();
-			// <12> 打印 Spring Boot 启动的时长日志。
+			//打印 Spring Boot 启动的时长日志。
 			if (this.logStartupInfo) {
 				new StartupInfoLogger(this.mainApplicationClass)
 						.logStarted(getApplicationLog(), stopWatch);
 			}
-			// <13> 通知 SpringApplicationRunListener 的数组，Spring 容器启动完成。
+			//通知 SpringApplicationRunListener 的数组，Spring 容器启动完成。
 			listeners.started(context);
-			// <14> 调用 ApplicationRunner 或者 CommandLineRunner 的运行方法。
+			// 调用 ApplicationRunner 或者 CommandLineRunner 的运行方法。
 			callRunners(context, applicationArguments);
 		}
 		catch (Throwable ex) {
-			// <14.1> 如果发生异常，则进行处理，并抛出 IllegalStateException 异常
+			//如果发生异常，则进行处理，并抛出 IllegalStateException 异常
 			handleRunFailure(context, ex, exceptionReporters, listeners);
 			throw new IllegalStateException(ex);
 		}
